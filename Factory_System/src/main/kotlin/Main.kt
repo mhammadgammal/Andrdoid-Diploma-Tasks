0b63f183c4845b5904681c46e0cb6c0000d37a1c
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2023-05-07 21:15:36 +0300

    creating a factory system

---
+++ Factory_System/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import java.util.*
+import kotlin.streams.toList
+
+fun main() {
+	val productList = mutableListOf<Product>()
+	val intScanner = Scanner(System.`in`)
+	for(i in 1..4){
+		println("Product #$i quantity")
+		val quantity = intScanner.nextInt()
+		println("Product #$i total sales")
+		val totalSales = intScanner.nextInt()
+		val product = Product(quantity = quantity, totalSales = totalSales)
+		productList.add(product)
+	}
+	
+	println(productList)
+	println("high total sales ${ productList.maxBy { product -> product.totalSales } }")
+	println("low quantities ${productList.minBy { product -> product.quantity }}")
+	
+}
